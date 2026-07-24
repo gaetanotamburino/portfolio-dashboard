ccdb3302c162264cc5f3f10e0be9558e00e0521c
tntmbr00 <gaetanotamburino@gmail.com>
2026-07-24 13:32:31 +0200

    Update open_dashboard.bat

--- open_dashboard.bat
+++ open_dashboard.bat
@@ -2,4 +2,4 @@
 cd /d "%~dp0"
 call %USERPROFILE%\anaconda3\Scripts\activate.bat %USERPROFILE%\anaconda3\a33
 python scripts\run_pipeline.py --live
-start "" "Portafogliov4.xlsm"
+start "" "portfolio-dashboard.xlsm"
